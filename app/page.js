c40832f0963595c38b5d332323430a3376a33ca0
prathamrasal <prathamrasal6@gmail.com>
2023-07-12 13:38:15 +0530

    Added Styles

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client"
 import Header from '@/components/Header'
-import Image from 'next/image'
 import { useState, useEffect } from 'react'
 
 
@@ -138,97 +137,105 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="container mx-auto my-8">
-        <div className='text-green-800 text-center'>{alert}</div>
-        <h1 className="text-3xl font-semibold mb-6">Search a Product</h1>
-        <div className="flex mb-2">
-          <input onChange={onDropdownEdit} type="text" placeholder="Enter a product name" className="flex-1 border border-gray-300 px-4 py-2 rounded-l-md" />
-          {/* <select className="border border-gray-300 px-4 py-2 rounded-r-md">
+      <div className='contain1'>
+        <div className="container mx-auto my-8">
+          <div className='text-green-800 text-center'>{alert}</div>
+          <h1 className="text-3xl font-semibold mb-6">Search a Product</h1>
+          <div className="flex mb-2">
+            <input onChange={onDropdownEdit} type="text" placeholder="Enter a product name" className="flex-1 border border-gray-300 px-4 py-2 rounded-l-md" />
+            {/* <select className="border border-gray-300 px-4 py-2 rounded-r-md">
             <option value="">All</option>
             <option value="category1">Category 1</option>
             <option value="category2">Category 2</option>
           </select> */}
-        </div>
-        {loading && <div className='flex justify-center items-center'> <img width={74} src="/loading.svg" alt="" /> </div>
-        }
-        <div className="dropcontainer absolute w-[72vw] border-1 bg-purple-100 rounded-md ">
+          </div>
+          {loading && <div className='flex justify-center items-center'> <img width={74} src="/loading.svg" alt="" /> </div>
+          }
+          <div className="dropcontainer absolute w-[72vw] border-1 bg-purple-100 rounded-md ">
 
-          {dropdown.map(item => {
-            return <div key={item.slug} className="container flex justify-between p-2 my-1 border-b-2">
+            {dropdown.map(item => {
+              return <div key={item.slug} className="container flex justify-between p-2 my-1 border-b-2">
 
-              <span className="slug"> {item.slug} ({item.quantity} available for ₹{item.price})</span>
-              <div className='mx-5'>
-                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"> - </button>
+                <span className="slug"> {item.slug} ({item.quantity} available for ₹{item.price})</span>
+                <div className='mx-5'>
+                  <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"> - </button>
 
-                <span className="quantity inline-block  min-w-3 mx-3">{item.quantity}</span>
-                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">  + </button>
+                  <span className="quantity inline-block  min-w-3 mx-3">{item.quantity}</span>
+                  <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add inline-block px-3 py-1 cursor-pointer bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">  + </button>
 
+                </div>
               </div>
-            </div>
-          })}
+            })}
+          </div>
         </div>
       </div>
+      <div className='contain'>
+        <div className='left'>
+          {/* Display Current Stock  */}
+          <div className="container mx-auto my-8">
+            <h1 className="text-3xl font-semibold mb-6">Add a Product</h1>
 
-      {/* Display Current Stock  */}
-      <div className="container mx-auto my-8">
-        <h1 className="text-3xl font-semibold mb-6">Add a Product</h1>
+            <form>
+              <div className="mb-4">
+                <label htmlFor="productName" className="block mb-2">Product Name</label>
+                <input value={productForm?.slug || ""} name='slug' onChange={handleChange} placeholder='Enter Product Name' type="text" id="productName" className="w-full border border-gray-300 px-4 py-2" />
+              </div>
 
-        <form>
-          <div className="mb-4">
-            <label htmlFor="productName" className="block mb-2">Product Slug</label>
-            <input value={productForm?.slug || ""} name='slug' onChange={handleChange} type="text" id="productName" className="w-full border border-gray-300 px-4 py-2" />
+              <div className="mb-4">
+                <label htmlFor="quantity" className="block mb-2">Quantity</label>
+                <input value={productForm?.quantity || ""} name='quantity' onChange={handleChange} type="number" placeholder='Enter Product Quantity' id="quantity" className="w-full border border-gray-300 px-4 py-2" />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="price" className="block mb-2">Price</label>
+                <input value={productForm?.price || ""} name='price' onChange={handleChange} type="number" id="price" placeholder='Enter Product Price' className="w-full border border-gray-300 px-4 py-2" />
+              </div>
+
+              <button onClick={addProduct} type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-lg shadow-md font-semibold">
+                Add Product
+              </button>
+
+
+            </form>
+          </div >
+
+        </div >
+
+
+        <div className="container  my-8 mx-auto">
+          <div className='right'>
+            <h1 className="text-3xl font-semibold mb-6">Display Current Stock</h1>
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2">Product Name</th>
+                  <th className="px-4 py-2">Quantity</th>
+                  <th className="px-4 py-2">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {console.log(products)}
+                {products?.map(product => {
+                  return <tr key={product.slug}>
+                    <td className="border px-4 py-2">{product.slug}</td>
+                    <td className="border px-4 py-2">{product.quantity}</td>
+                    <td className="border px-4 py-2">₹{product.price}</td>
+                    <td className="border px-4 py-2">
+                      <button
+                        onClick={() => buttonAction("delete", product.slug, product.quantity)}
+                        disabled={loadingaction}
+                        className="delete inline-block px-1 py-1 cursor-pointer bg-red-500 text-white font-semibold rounded-lg shadow-md disabled:bg-red-200"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                })}
+
+              </tbody>
+            </table>
           </div>
-
-          <div className="mb-4">
-            <label htmlFor="quantity" className="block mb-2">Quantity</label>
-            <input value={productForm?.quantity || ""} name='quantity' onChange={handleChange} type="number" id="quantity" className="w-full border border-gray-300 px-4 py-2" />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="price" className="block mb-2">Price</label>
-            <input value={productForm?.price || ""} name='price' onChange={handleChange} type="number" id="price" className="w-full border border-gray-300 px-4 py-2" />
-          </div>
-
-          <button onClick={addProduct} type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-lg shadow-md font-semibold">
-            Add Product
-          </button>
-
-
-        </form>
-      </div>
-      <div className="container my-8 mx-auto">
-        <h1 className="text-3xl font-semibold mb-6">Display Current Stock</h1>
-
-        <table className="table-auto w-full">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Product Name</th>
-              <th className="px-4 py-2">Quantity</th>
-              <th className="px-4 py-2">Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {console.log(products)}
-            {products?.map(product => {
-              return <tr key={product.slug}>
-                <td className="border px-4 py-2">{product.slug}</td>
-                <td className="border px-4 py-2">{product.quantity}</td>
-                <td className="border px-4 py-2">₹{product.price}</td>
-                <td className="border px-4 py-2">
-                  <button
-                    onClick={() => buttonAction("delete", product.slug, product.quantity)}
-                    disabled={loadingaction}
-                    className="delete inline-block px-3 py-1 cursor-pointer bg-red-500 text-white font-semibold rounded-lg shadow-md disabled:bg-red-200"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            })}
-
-          </tbody>
-        </table>
-
+        </div>
       </div>
     </>
   )
